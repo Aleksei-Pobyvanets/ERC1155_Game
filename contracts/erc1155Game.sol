0b6de3e4ae308bee9f1047b6d8dc9f7c757b946b
Aleksei <kilroygood@gmail.com>
2022-12-04 16:21:03 +0200

    add allow list func

--- contracts/erc1155Game.sol
+++ contracts/erc1155Game.sol
@@ -12,6 +12,10 @@ contract ERC1155Game is ERC1155, Ownable, Pausable, ERC1155Supply {
     uint public allowListPrice = 0.005 ether;
     uint public maxSupply = 10;
 
+    bool public publicMintOpen = false;
+    bool public allowListMintOpen = true;
+
+    mapping(address => bool) allowList;
 
     constructor() ERC1155("") {}
 
@@ -28,17 +32,29 @@ contract ERC1155Game is ERC1155, Ownable, Pausable, ERC1155Supply {
     }
 
     function allowList(uint id, uint amount) public payable{
-        
+        require(allowListMintOpen, "allowList Mint is Closed!");
+        require(allowList[msg.sender], "Tou are not on the Allow list!");
+        require(id < 3, "Wrong ID!");
+        require(msg.value == allowListPrice * amount, "insufficient funds!");
+        require(totalSupply(id) + amount <= maxSupply, "Sorry we have ninted out!");
+
+        _mint(msg.sender, id, amount, "");
+    }
+    function setAllowList(address[] calldata addreses) external onlyOwner{
+        for(uint256 i = 0; i < addreses.length; i++){
+            allowList[addreses[i]] = true;
+        }
     }
 
-    function mint( uint256 id, uint256 amount, bytes memory data)
+    function publicMint( uint256 id, uint256 amount)
         public
         payable
     {
+        require(publicMintOpen, "Public Mint is Closed!");
         require(id < 3, "Wrong ID!");
         require(msg.value == pyblicPrice * amount, "insufficient funds!");
         require(totalSupply(id) + amount <= maxSupply, "Sorry we have ninted out!");
-        _mint(msg.sender, id, amount, data);
+        _mint(msg.sender, id, amount, "");
     }
 
     function uri(uint _id)public view virtual override returns(string memory){
@@ -60,6 +76,11 @@ contract ERC1155Game is ERC1155, Ownable, Pausable, ERC1155Supply {
         override(ERC1155, ERC1155Supply)
     {
         super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    function editMintWindows(bool _allowListMintOpen, bool _publicMintOpen) external onlyOwner{
+        publicMintOpen = _publicMintOpen;
+        allowListMintOpen = _allowListMintOpen;
     }
 
     function withdraw(address _addr) external onlyOwner{
